3760e4e3c4d5d0051b04268ba525c09a6f6041c1
Moricchi <ptnadia337@gmail.com>
2023-11-22 17:44:08 +0800

    ubah input requirednya di CREATORS

--- Submission.php
+++ Submission.php
@@ -1,26 +1,29 @@
 <?php
-if(isset($_POST["submit"])){
-    $name = $_POST['name'];
-    $date = $_POST['date'];
-    $category = $_POST['category'];
-    $type = $_POST['type'];
-    $concentration = $_POST['concentration'];
-    $inolink = $_POST['inolink'];
-    $youtube = $_POST['youtube'];
-    $images = $_POST['images'];
-    $description = $_POST['description'];
-    $group = $_POST['group'];
-    $id = $_POST['id'];
-    $email = $_POST['email'];
-    }
+
+print_r($_POST);
+
+//if(isset($_POST["submit"])){
+//    $name = $_POST['name'];
+//    $date = $_POST['date'];
+//    $category = $_POST['category'];
+//    $type = $_POST['type'];
+//    $concentration = $_POST['concentration'];
+//    $inolink = $_POST['inolink'];
+//    $youtube = $_POST['youtube'];
+//    $images = $_POST['images'];
+//    $description = $_POST['description'];
+//    $group = $_POST['group'];
+//    $id = $_POST['id'];
+//    $email = $_POST['email'];
+//    }
 
     //Database Connection
-    $conn = new mysqli('localhost','root','','innovationsystemdb');
+//    $conn = new mysqli('localhost','root','','innovationsystemdb');
     
-    $query = "INSERT INTO innovationsystemdb.sql VALUES('', '$name', '$date', '$category', '$type', '$concentration', '$inolink', '$youtube', '$images', '$description', '$group', '$id', '$email')";
-    mysqli_query($conn, $query);
-    echo
-    "
-    <script> alert('Your innovation has been submitted succesfully'); </script>
-    ";
+//    $query = "INSERT INTO innovationsystemdb.sql VALUES('', '$name', '$date', '$category', '$type', '$concentration', '$inolink', '$youtube', '$images', '$description', '$group', '$id', '$email')";
+//    mysqli_query($conn, $query);
+//    echo
+//    "
+//    <script> alert('Your innovation has been submitted succesfully'); </script>
+//    ";
 ?>
