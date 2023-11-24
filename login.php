2c4d99c5378a30f1e768d032e527f20481e9824d
Moricchi <ptnadia337@gmail.com>
2023-11-24 17:52:49 +0800

    login successfully
    login update database successfully created.

--- login.php
+++ login.php
@@ -1,3 +1,31 @@
 <?php
+$host = "localhost";
+$IDUser = "root";
+$UserPass = "";
 
+$conn = new mysqli('localhost','root','','innovationsystemdb');
+
+if(mysqli_connect_error()){
+    die('Failed to Login('. mysqli_connect_errno() .')'. mysqli_connect_error());
+}
+else{
+    $sql = "INSERT INTO user (IDUser, UserPass)
+    VALUES ('$IDUser','$UserPass')";
+    if($conn->query($sql)){
+        echo "Login Sucessfully";
+    }
+    else{
+        echo "Login Failed". $sql ."<br>". $conn->error;
+    }
+    $conn->close();
+}
+
+
+//if(isset($_POST["submit"])){
+//    $IDUser = $_POST['IDUser'];
+//    $UserPass = $_POST['UserPass'];
+//}
+
+//$query = "INSERT INTO user (IDUser, UserPass) VALUES ($IDUser, $UserPass)";
+//$upload= mysqli_query($conn, $query);
 ?>
