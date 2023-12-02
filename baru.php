c39aded67c8b95e5839f638b1b5536831fbae450
lialialiashymfony <151517394+lialialiashymfony@users.noreply.github.com>
2023-12-02 23:33:16 +0800

    hehe ini yg fix ya, up image udah bisa lebih dari 1 :), cuman data yg ke up masi dobel hehe

--- baru.php
+++ baru.php
@@ -9,17 +9,48 @@ if (isset($_POST['submit'])) {
     $Description = $_POST['Description'];
     $SubmDate = $_POST['SubmDate'];
     $CreDate = $_POST['CreDate'];
-    $Img = $_FILES['Img']['name'];
+
+    $ImgArray = [];
+    if (is_array($_FILES['Img']['name'])) {
+        $totalFiles = count($_FILES['Img']['name']);
+
+        for ($i = 0; $i < $totalFiles; $i++) {
+            $Img = $_FILES['Img']['name'][$i];
+            $location = "image/" . $Img;
+            move_uploaded_file($_FILES['Img']['tmp_name'][$i], $location);
+            $ImgArray[] = $Img;
+        }
+        $ImgString = implode(",", $ImgArray);
+    } else {
+        $Img = $_FILES['Img']['name'];
+        $location = "image/" . $Img;
+        move_uploaded_file($_FILES['Img']['tmp_name'], $location);
+        $ImgString = $Img;
+    }
+    /*else {
+        // Handle the case when only one file is uploaded
+        $Img = $_FILES['Img']['name'];
+        $location = "image/" . $Img;
+        move_uploaded_file($_FILES['Img']['tmp_name'], $location);
+        $ImgArray[] = $Img;
+    }
+    /*$Img = $_FILES['Img']['name'];
     $location = "image/" . $Img;
-    move_uploaded_file($_FILES['Img']['tmp_name'], $location); // Fix typo here
+    move_uploaded_file($_FILES['Img']['tmp_name'], $location); // Fix typo here */
+
+
+
     $Link = $_POST['Link'];
     $LinkYT = $_POST['LinkYT'];
     $IDConc = $_POST['IDConc'];
     $IDCateg = $_POST['IDCateg'];
     $IDType = $_POST['IDType'];
     $id_user_array = $_POST['IDUser'];
+
+
     $query = "INSERT INTO innovdata (NameInnov, Description, Status, SubmDate, CreDate, Link, Img, LinkYoutube, IDConc, IDCateg, IDType) VALUES
-     ('$NameInnov','$Description','Pending','$SubmDate','$CreDate','$Link','$Img', '$LinkYT', '$IDConc', '$IDCateg', '$IDType')"; // Fix column name here
+     ('$NameInnov','$Description','Pending','$SubmDate','$CreDate','$Link','$ImgString', '$LinkYT', '$IDConc', '$IDCateg', '$IDType')"; // Fix column name here
+
     $result = mysqli_query($koneksi, $query);
 
     if (mysqli_query($koneksi, $query)) {
@@ -37,13 +68,13 @@ if (isset($_POST['submit'])) {
         echo "<script>alert('errors'); </script>";
     }
 
-    /*if (!$result) {
+    if (!$result) {
         echo "<script>alert('errors'); </script>";
     } else {
         echo "<script>alert('success'); </script>"; // Fix typo here
-    }*/
+    }
 }
-mysqli_close($koneksi);
+
 
 ?>
 
@@ -62,25 +93,35 @@ mysqli_close($koneksi);
     <form method="POST" action="" enctype="multipart/form-data">
         <table>
             <tr>
-                <td>Nama Innovasi</td>
+                <div>
+                    Submission Form
+                </div>
+            </tr>
+            <tr>
+                <td><label for="NameInnov"> Innovation Name </label></td>
                 <td><input type="text" name="NameInnov" placeholder="Input your innovation name"></td>
             </tr>
             <tr>
-                <td>Deskripsi</td>
-                <td><input type="text" name="Description" placeholder="Add description here"></td>
+                <td><label for="Description">Description </label></td>
+                <td><textarea name="Description" placeholder="Add description here"></textarea></td>
             </tr>
             <tr>
-                <td>Submit Date</td>
+                <td><label for="SubmDate">Submit Date</label></td>
                 <td><input type="date" name="SubmDate"></td>
             </tr>
             <tr>
-                <td>Create Date</td>
+                <td><label for="CreDate">Create Date</label></td>
                 <td><input type="date" name="CreDate"></td>
             </tr>
-            <tr>
-                <td>Image</td>
-                <td><input type="file" name="Img" accept="image/*"></td>
-            </tr>
+            <div style="display: none;">
+                <input type="file" name="Img[]" id="fileInput" multiple onchange="updateList()">
+            </div>
+            <tbody>
+                <tr>
+                    <td><input type="button" value="Choose Image" onclick="document.getElementById('fileInput').click()">
+                        <div id="fileList"></div>
+                </tr>
+            </tbody>
             <tr>
                 <td>Link</td>
                 <td><input type="link" name="Link" placeholder="Input your Link"></td>
@@ -90,51 +131,95 @@ mysqli_close($koneksi);
                 <td><input type="link" name="LinkYT" placeholder="Input your Link Youtube"></td>
             </tr>
             <tr>
-                Concentration:
-                <select name="IDConc">
-                    <option value="1"> Cyber Security</option>
-                    <option value="2"> Management Information</option>
-                    <option value="3"> Engineering and Bussiness</option>
-                    <option value="4"> Others</option>
-                </select><br>
+                <td>Concentration: </td>
+                <td><select name="IDConc">
+                        <option value="1"> Cyber Security</option>
+                        <option value="2"> Management Information</option>
+                        <option value="3"> Engineering and Bussiness</option>
+                        <option value="4"> Others</option>
+                    </select></td>
             </tr>
             <tr>
-                Category:
-                <select name="IDCateg">
-                    <option value="1"> Thesis</option>
-                    <option value="2"> Internship</option>
-                    <option value="3"> Others</option>
-                </select><br>
+                <td>Category: </td>
+                <td><select name="IDCateg">
+                        <option value="1"> Thesis</option>
+                        <option value="2"> Internship</option>
+                        <option value="3"> Others</option>
+                    </select></td>
             </tr>
             <tr>
-                Type:
-                <select name="IDType">
-                    <option value="1"> Website</option>
-                    <option value="2"> Desktop</option>
-                    <option value="3"> MobileApp</option>
-                    <option value="4"> Others</option>
-                </select><br>
+                <td>Type: </td>
+                <td><select name="IDType">
+                        <option value="1"> Website</option>
+                        <option value="2"> Desktop</option>
+                        <option value="3"> MobileApp</option>
+                        <option value="4"> Others</option>
+                    </select></td>
             </tr>
-            <div id="user">
-                <div class="user">
-                    NIM/NIDN: <input type="text" name="IDUser[]"><br>
-                </div>
-            </div>
-            <button type="button" onclick="addUser()"> Add more </button><br>
+            <tbody id="user">
+                <tr class="user">
+                    <td>NIM/NIDN: </td>
+                    <td><input type="text" name="IDUser[]"></td>
+                    <td><button type="button" onclick="removeUser(this)">Delete</button></td>
+                </tr>
+            </tbody>
+            </tbody>
+
+            <td><button type="button" onclick="addUser()"> Add more </button></td>
+
             <tr>
-                <td><input type="submit" value="submit" name="submit" onclick="this.disabled=true; this.form.submit();"></td>
+                <td><input type="submit" value="submit" name="submit"></td>
             </tr>
         </table>
     </form>
     <script>
         function addUser() {
             var userContainer = document.getElementById('user');
-            var newUser = document.createElement('div');
+            var newUser = document.createElement('tr');
             newUser.className = 'user';
-            newUser.innerHTML = 'NIM/NIDN:<input type="text" name="IDUser[]"><br>';
+            newUser.innerHTML = '<td>NIM/NIDN: </td><td><input type="text" name="IDUser[]"></td>';
+            var removeButton = document.createElement('button');
+            removeButton.type = 'button';
+            removeButton.textContent = 'Delete';
+            removeButton.onclick = function() {
+                removeUser(this);
+            }
+            newUser.appendChild(removeButton);
+            /*var lastUser = userContainer.lastElementChild;*/
             userContainer.appendChild(newUser);
         }
-    </script>
-</body>
 
-</html>
+        function removeUser(button) {
+            var userContainer = document.getElementById('user');
+            var userDiv = button.parentNode;
+            userContainer.removeChild(userDiv);
+        }
+
+        function updateList() {
+            var input = document.getElementById('fileInput');
+            var output = document.getElementById('fileList');
+            var files = input.files;
+
+            output.innerHTML = '';
+            for (var i = 0; i < files.length; i++) {
+                output.innerHTML += '<p>' + files[i].name + '</p>';
+            }
+        }
+
+        /*function addImage() {
+            var inputContainer = document.getElementById('imageContainer');
+            var newInput = document.createElement('input');
+            newInput.type = 'file';
+            newInput.name = 'Img[]';
+            inputContainer.appendChild(newInput);
+        }
+
+        function removeImage() {
+            var inputContainer = document.getElementById('imageContainer');
+            var lastInput = inputContainer.lastElementChild;
+            if (lastInput) {
+                inputContainer.removeChild(lastInput);
+            }
+        }*/
+    </script>
+    <style>
