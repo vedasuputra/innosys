23e8c0c1feaf6b323c18ceb41d400881e0b86341
vedasuputra <151516527+vedasuputra@users.noreply.github.com>
2023-12-25 16:22:44 +0800

    Update help.php

--- help.php
+++ help.php
@@ -36,39 +36,19 @@ include "connect.php";
 
     <div class="innovation-creators">
       <div class="filters-container">
-        <button class="accordion">Type</button>
+        <button class="accordion">Question 1</button>
         <div class="panel">
           <div class="filter-container">
-            a
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus venenatis mi, vel consectetur urna hendrerit eu. Sed ullamcorper sapien ut justo vestibulum, a hendrerit ligula malesuada.
           </div>
         </div>
       </div>
 
       <div class="filters-container">
-        <button class="accordion">Type</button>
+        <button class="accordion">Question 2</button>
         <div class="panel">
           <div class="filter-container">
-            a
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="innovation-creators">
-      <div class="filters-container">
-        <button class="accordion">Type</button>
-        <div class="panel">
-          <div class="filter-container">
-            a
-          </div>
-        </div>
-      </div>
-
-      <div class="filters-container">
-        <button class="accordion">Type</button>
-        <div class="panel">
-          <div class="filter-container">
-            a
+            Nulla facilisi. Fusce at ligula nec dui mattis cursus. Sed auctor eros ut nunc fermentum, vel efficitur nulla consequat. Phasellus ultricies ligula in purus bibendum, non ultricies neque blandit.
           </div>
         </div>
       </div>
@@ -76,19 +56,19 @@ include "connect.php";
 
     <div class="innovation-creators">
       <div class="filters-container">
-        <button class="accordion">Type</button>
+        <button class="accordion">Question 3</button>
         <div class="panel">
           <div class="filter-container">
-            a
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In hac habitasse platea dictumst. Suspendisse feugiat lectus vitae diam aliquet, sit amet convallis justo ultrices.
           </div>
         </div>
       </div>
 
       <div class="filters-container">
-        <button class="accordion">Type</button>
+        <button class="accordion">Question 4</button>
         <div class="panel">
           <div class="filter-container">
-            a
+            Quisque id massa in nisl bibendum dapibus. Maecenas rhoncus erat nec justo ultrices, vel lacinia ligula facilisis. Nunc fermentum justo id augue consequat, vel tincidunt orci accumsan.
           </div>
         </div>
       </div>
@@ -96,19 +76,39 @@ include "connect.php";
 
     <div class="innovation-creators">
       <div class="filters-container">
-        <button class="accordion">Type</button>
+        <button class="accordion">Question 5</button>
         <div class="panel">
           <div class="filter-container">
-            a
+            Aliquam non feugiat purus. Sed suscipit felis vitae turpis sodales, id aliquet felis dictum. Integer tristique, dolor vel fermentum mattis, arcu velit consectetur justo, in euismod lacus justo sit amet nulla.
           </div>
         </div>
       </div>
 
       <div class="filters-container">
-        <button class="accordion">Type</button>
+        <button class="accordion">Question 6</button>
         <div class="panel">
           <div class="filter-container">
-            a
+            Etiam scelerisque justo et justo varius ultricies. Curabitur quis nisl sed est dictum iaculis vel vitae neque. Sed ullamcorper nisl eu urna fringilla, eu lacinia odio congue.
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="innovation-creators">
+      <div class="filters-container">
+        <button class="accordion">Question 7</button>
+        <div class="panel">
+          <div class="filter-container">
+            Fusce a elit in dolor vestibulum lacinia. Pellentesque quis ipsum vel tortor cursus luctus. Suspendisse eget risus eu ligula tincidunt gravida.
+          </div>
+        </div>
+      </div>
+
+      <div class="filters-container">
+        <button class="accordion">Question 8</button>
+        <div class="panel">
+          <div class="filter-container">
+            Integer cursus justo eu tincidunt aliquet. Vivamus ac quam nec ex cursus varius. Aliquam quis nunc vel justo pulvinar fermentum.
           </div>
         </div>
       </div>
